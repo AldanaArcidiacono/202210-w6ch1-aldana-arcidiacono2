409f8809454b50f7203b001406393f1ae1a74eb0
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-08 19:14:46 +0100

    Adding type Characters

---
+++ src/features/models/data.ts
@@ -0,0 +1,11 @@
+export type Characters = {
+    role: string;
+    id: number;
+    name: string;
+    family: string;
+    age: number;
+    lifeStatus: boolean;
+    message: string;
+    image: string;
+    series: string;
+};
